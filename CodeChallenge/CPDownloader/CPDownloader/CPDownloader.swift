e96f096963b2934cb7dfe94b2d7323c521d70c79
Jose Torres <jose.torres@santexgroup.com>
2016-12-29 19:14:40 -0500

    Created base classes for DownloadManager

---
+++ CodeChallenge/CPDownloader/CPDownloader/CPDownloader.swift
@@ -0,0 +1,10 @@
+//
+//  CPDownloader.swift
+//  CPDownloader
+//
+//  Created by Santex on 12/29/16.
+//  Copyright © 2016 coderpug. All rights reserved.
+//
+
+import Foundation
+
---
+++ CodeChallenge/CPDownloader/CPDownloader/DownloadManager/DownloadManager.swift
@@ -0,0 +1,9 @@
+//
+//  DownloadManager.swift
+//  CPDownloader
+//
+//  Created by Santex on 12/29/16.
+//  Copyright © 2016 coderpug. All rights reserved.
+//
+
+import Foundation
