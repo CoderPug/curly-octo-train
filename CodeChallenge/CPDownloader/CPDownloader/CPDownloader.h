709cf73e38bdd31cba80a092f0fef651d5adfcd6
Jose Torres <jose.torres@santexgroup.com>
2016-12-29 19:05:31 -0500

    Setup base project structure.

---
+++ CodeChallenge/CPDownloader/CPDownloader/CPDownloader.h
@@ -0,0 +1,19 @@
+//
+//  CPDownloader.h
+//  CPDownloader
+//
+//  Created by Santex on 12/29/16.
+//  Copyright © 2016 coderpug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CPDownloader.
+FOUNDATION_EXPORT double CPDownloaderVersionNumber;
+
+//! Project version string for CPDownloader.
+FOUNDATION_EXPORT const unsigned char CPDownloaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CPDownloader/PublicHeader.h>
+
+
