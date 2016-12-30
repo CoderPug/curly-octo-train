ba519df6252ac6e0d10a38dbd925168c548edb08
Jose Torres <jose.torres@santexgroup.com>
2016-12-30 11:08:46 -0500

    Added InstagramTableViewCell view class.

---
+++ CodeChallenge/CodeChallenge/InstagramImages/Views/InstagramTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTableViewCell.swift
+//  CodeChallenge
+//
+//  Created by Santex on 12/30/16.
+//  Copyright © 2016 coderpug. All rights reserved.
+//
+
+import UIKit
+
+class InstagramTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
