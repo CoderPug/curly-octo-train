c296cc02d0a969e5885ebc0df53009a8f44d87f2
Jose Torres <jose.torres@santexgroup.com>
2016-12-30 10:25:30 -0500

    Renamed ViewController to InstagramImagesViewController. Setup base tabbar navigation.

---
+++ CodeChallenge/CodeChallenge/InstagramImagesViewController.swift
@@ -0,0 +1,24 @@
+//
+//  InstagramImagesViewController.swift
+//  CodeChallenge
+//
+//  Created by Jose Torres on 12/29/16.
+//  Copyright © 2016 coderpug. All rights reserved.
+//
+
+import UIKit
+import CPDownloader
+
+class InstagramImagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
