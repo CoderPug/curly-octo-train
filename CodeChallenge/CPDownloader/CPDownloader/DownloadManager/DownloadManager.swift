f1bb35c8a3077c041c0f1c4a3ff97c1e9b72f342
Jose Torres <jose.torres@santexgroup.com>
2016-12-29 20:42:56 -0500

    Added DownloadDataManager extension to support download of images.

--- CodeChallenge/CPDownloader/CPDownloader/DownloadManager/DownloadManager.swift
+++ CodeChallenge/CPDownloader/CPDownloader/DownloadManager/DownloadManager.swift
@@ -12,7 +12,7 @@ import Foundation
 ///
 /// - Failure: In case response is a failure. It contains the descriptive failure error.
 /// - Success: In case response is a success. It contains <T>.
-enum Result<T> {
+public enum Result<T> {
     case Failure(Error)
     case Success(T)
 }
@@ -33,17 +33,19 @@ enum SupportedTypes {
 /// - unknown: unhandled error.
 /// - requestFailed: request failed.
 /// - couldNotReadResponse: request succeeded but response data can not be read.
+/// - couldNotObtainImage: request succeeded but response data can not be converted to image.
 enum DownloadManagerError: Error {
     case unknown
     case wrongURL
     case requestFailed(Error?)
     case couldNotReadResponse(Error?)
+    case couldNotObtainImage
 }
 
-struct DownloadDataManager {
+public struct DownloadDataManager {
     
     fileprivate typealias downloadDataManagerResponseType = (data: Data?, type: SupportedTypes)
-    fileprivate typealias downloadDataManagerHadler = (Result<downloadDataManagerResponseType>) -> Swift.Void
+    fileprivate typealias downloadDataManagerHandler = (Result<downloadDataManagerResponseType>) -> Swift.Void
     
     fileprivate static let kDownloadDataManagerHeaderContentType = "Content-Type"
     fileprivate static let kDownloadDataManagerImageContentType = "image/"
@@ -54,7 +56,7 @@ struct DownloadDataManager {
     /// - Parameters:
     ///   - url: String url
     ///   - handler: Handler for response
-    fileprivate static func download(url: String, handler: @escaping downloadDataManagerHadler) {
+    fileprivate static func download(url: String, handler: @escaping downloadDataManagerHandler) {
         
         guard let url = URL(string: url) else {
             
@@ -93,20 +95,58 @@ struct DownloadDataManager {
     ///   - data: Data of response's data
     ///   - type: String containing response's Content-Type
     ///   - handler: Handler for response
-    fileprivate static func interprete(_ data: Data?, with type: String, handler: downloadDataManagerHadler) {
+    fileprivate static func interprete(_ data: Data?, with type: String, handler: downloadDataManagerHandler) {
         
         switch type {
 
         case _ where type.contains(kDownloadDataManagerImageContentType):
+            
             handler(.Success((data, .image)))
             break
             
         case _ where type.contains(kDownloadDataManagerJSONContentType):
+            
             handler(.Success((data, .json)))
+            break
             
         default:
+            
             handler(.Success((data, .unknown)))
             break
         }
     }
+}
+
+//  MARK: DownloadDataManager+UIImage
+
+extension DownloadDataManager {
+
+    public typealias downloadDataManagerImageHandler = (Result<UIImage>) -> Swift.Void
+    
+    public static func downloadImage(url: String, handler: @escaping downloadDataManagerImageHandler) {
+    
+        download(url: url) { result in
+        
+            switch result {
+                
+            case let .Failure(error):
+                
+                handler(.Failure(error))
+                break
+                
+            case let .Success(data, type):
+                
+                guard type == .image,
+                    let data = data,
+                    let image = UIImage(data: data) else {
+                        
+                        handler(.Failure(DownloadManagerError.couldNotObtainImage))
+                        return
+                }
+                handler(.Success(image))
+                break
+            }
+        }
+    }
+    
 }
