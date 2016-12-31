485fe577ae7db2dc7567c6dcd0a7efeab54d1d97
Jose Torres <jose.torres@santexgroup.com>
2016-12-31 00:54:06 -0500

    Added OtherTypesTableViewController class.

---
+++ CodeChallenge/CodeChallenge/OtherTypes/OtherTypesTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  OtherTypesTableViewController.swift
+//  CodeChallenge
+//
+//  Created by Jose Torres on 12/31/16.
+//  Copyright © 2016 coderpug. All rights reserved.
+//
+
+import UIKit
+
+class OtherTypesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
