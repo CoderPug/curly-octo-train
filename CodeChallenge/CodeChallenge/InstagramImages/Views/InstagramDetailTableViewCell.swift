5228f5562a1dc757dad02b6a59323d36745c2ffe
Jose Torres <jose.torres@santexgroup.com>
2016-12-31 00:43:00 -0500

    Loading data information in InstagramDetailTableViewCell.

--- CodeChallenge/CodeChallenge/InstagramImages/Views/InstagramDetailTableViewCell.swift
+++ CodeChallenge/CodeChallenge/InstagramImages/Views/InstagramDetailTableViewCell.swift
@@ -32,6 +32,31 @@ class InstagramDetailTableViewCell: UITableViewCell {
     
     func custommize() {
         
+        backgroundColor = UIColor.clear
+        
+        if #available(iOS 8.2, *) {
+            labelAuthor.font = Appearance.Fonts.h4
+            labelDate.font = Appearance.Fonts.text
+            labelDescription.font = Appearance.Fonts.text
+        }
+        
+        labelAuthor.textColor = Appearance.Colors.third
+        labelDate.textColor = Appearance.Colors.third
+        labelDescription.textColor = Appearance.Colors.third
+    }
+    
+    func load(data: [String: AnyObject]) {
+        
+        guard let author = (data as NSDictionary).value(forKeyPath: "caption.from.full_name") as? String,
+            let description = (data as NSDictionary).value(forKeyPath: "caption.text") as? String,
+            let date = (data as NSDictionary).value(forKeyPath: "caption.created_time") as? String else {
+            
+            return
+        }
+        
+        labelAuthor.text = author
+        labelDate.text = date
+        labelDescription.text = description
     }
 
 }
